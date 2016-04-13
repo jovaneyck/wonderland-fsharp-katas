f070de910a65e97d4c954e6a46b16bdec9f9052a
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-04-13 21:06:20 +0200

    first implementation of alphabet cipher. Not happy with decipher logic atm.

--- wonderland-fsharp-katas/alphabet-cipher.fsx
+++ wonderland-fsharp-katas/alphabet-cipher.fsx
@@ -3,14 +3,86 @@
 type Message = string
 type Keyword = string
 
-let encode (key:Keyword) (message:Message) : Message =
-    "encodeme"
+let allLetters =
+    [
+        for c in int('a')..int('z') do
+        yield char(c)
+    ]
 
-let decode (key:Keyword) (message:Message) : Message =
-    "decodeme"
+let rec infinite alphabet = 
+    seq {
+        for l in alphabet do yield l
+        for l in infinite alphabet do yield l
+    }
+
+let toIndex c = (int c) - (int 'a')
+let fromIndex c = int('a') + c |> char
+
+let lettersStartingWith letter =
+    infinite allLetters
+    |> Seq.skip (toIndex letter)
+
+let encodeLetter secretletter letter =
+    lettersStartingWith letter
+    |> Seq.item (toIndex secretletter)
+
+let decodeLetter letter secretLetter = 
+    let (indexOfLetter, _) = 
+        (lettersStartingWith secretLetter)
+        |> Seq.indexed
+        |> Seq.find (fun (idx, l) -> l = letter)
+    indexOfLetter |> fromIndex
+
+let translate lettertranslator key (message : Message) =
+    let secret = infinite key |> Seq.take (message.Length)
+    let secretAndMessage = secret |> Seq.zip message
+
+    secretAndMessage
+    |> Seq.map (fun (s,m) -> lettertranslator s m)
+    |> Array.ofSeq
+    |> Message
+
+let encode : (Keyword -> Message -> Message) = translate encodeLetter
+
+let decode : (Keyword -> Message -> Message) = translate decodeLetter
+
+let crack msgLetter cipherLetter =
+    let(indexOfCipher,_) =
+        lettersStartingWith msgLetter
+        |> Seq.indexed
+        |> Seq.find (fun (_, l) -> l = cipherLetter)
+    indexOfCipher |> fromIndex
+
+let toString (charArray : char list) = System.String.Join("",charArray)
+let startswith substring text = 
+    (text |> toString).StartsWith(substring |> toString)
+
+let rec isConstructedByRepeating (subset : char list) (text : char list) = 
+    if text.Length <= subset.Length
+    then subset |> startswith text
+    else 
+        text |> startswith subset 
+        && text |> List.skip(subset.Length) |> isConstructedByRepeating subset
+
+let largestRepeatingSubset allchars = 
+    let rec lrs acc (chars : char list) = 
+        match chars with
+        | [] -> acc
+        | h :: t -> 
+            let subset = acc @ [h]
+            if allchars |> isConstructedByRepeating subset
+            then subset
+            else lrs subset t
+    lrs [] allchars
 
 let decipher (cipher:Message) (message:Message) : Keyword =
-    "decypherme"
+    message
+    |> Seq.zip cipher
+    |> Seq.map (fun (c, m) -> crack m c)
+    |> Seq.toList
+    |> largestRepeatingSubset
+    |> Array.ofSeq
+    |> Keyword
 
 #r @"../packages/Unquote/lib/net45/Unquote.dll"
 open Swensen.Unquote
